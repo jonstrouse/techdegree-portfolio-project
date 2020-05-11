9a9a5e6d806b18ae495e9d66c803acfebb818e12
CodeJS <jonstrouse@gmail.com>
2020-05-11 08:06:29 -0400

    Submit Final Changes

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,11 +1,11 @@
 const minMenu = document.querySelector(".navbar-toggler");
 const projects = document.querySelector("#projects");
-const projectDiv = document.querySelectorAll('.project');
 const cardText = document.getElementsByClassName("card-text");
 const projectDetails = document.getElementsByClassName("project-details");
 
 
 // navigation background transition
+
 minMenu.addEventListener('click', () => {
   $(".navbar").css("background","rgb(20,55,70)");
   $(".navbar").css("padding-top",".5rem");
@@ -33,13 +33,6 @@ $(window).on("scroll",function(){
 });
 });
 
-
-// project card animation
-const arrow = document.getElementsByClassName("see-more");
-
-for(var i = 0; i < projectDiv.length; i ++ ) {
-    projectDiv[i].onclick = function() {
-      console.log(event);
-      arrow[i].style.transform = "rotate(180deg)";
-    };
-  };
+if($('.project').css.height > 300) {
+  $('.see-more').css('display', 'none');
+}
